9e4f1e88092e7275ad2b27cfff524134249f0eb6
kiera1512 <tien.td.1512@gmail.com>
2022-02-07 13:10:33 +0700

    3rd change

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,22 @@
+// $(document).ready(function(){
+//   getWeather();
+// })
 
-function myFunction() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+function getWeather(searchQuery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+ apiKey;
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
-
-  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+  $(".city").text("");
+  $(".temp").text("");
+ 
+  $.ajax(url,{success: function(data){
+      $(".city").text(data.name);
+      $(".temp").text(data.main.temp);
+  }, error: function(error){
+      $(".error-message").text("An Error!!!")
+  }})
 }
 
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
+}
